07990c0dc0b596bb51cc456e16b7d68ca5a1aa3c
Luiz Ferreira <luiz.ferreira@outlook.com>
2025-04-07 09:47:45 -0300

    fix: binding

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Lê a porta do ambiente (Render usa $PORT)
+    uvicorn.run(app, host="0.0.0.0", port=port) 
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from app.main import app
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port) 
